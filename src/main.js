55b235e67ae22e156f0b7d10acf3ebeb459a8c9f
Lsw7 <809568994@qq.com>
2020-11-05 11:37:52 +0800

    结构搭建完成

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+// 加载全局样式（最好放到最后，方便去覆盖第三方样式）
+import './styles/index.less'
+// 加载组件要用名字， 加载样式表直接用地址就行
+
 Vue.config.productionTip = false
 
 new Vue({
+  // 创建根实例,把router，store配置到根实例中
+  // 把APPue组件渲染到主页index中的id=app里
   router,
   store,
   render: h => h(App)
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {}
 })
+
+// VueX Vue状态管理模式，作用是 让组件之间共享数据更方便快捷
+// 如果组件如果有需要共享的数据，可以直接挂载到VueX实例中，而不必通过父子组件之间的传值了
+// 状态管理有5个核心，分别是state、mutation、getter、action、module
+// state简单的理解，可以把state想象成组件中的data，专门用来存储数据的。
+// 每一个 Vuex 应用的核心就是 store（仓库）
+// store就是一个容器，它包含着你的应用中大部分的状态 (state)
+// Vuex 的状态存储是响应式的，当 Vue 组件从 store 中读取状态的时候
+// 若 store 中的状态发生变化，那么相应的组件也会相应地得到高效更新
+// 你不能直接改变 store 中的状态。改变 store 中的状态的唯一途径就是显式地提交 (commit) mutation
+// 这样使得我们可以方便地跟踪每一个状态的变化，从而让我们能够实现一些工具帮助我们更好地了解我们的应用
